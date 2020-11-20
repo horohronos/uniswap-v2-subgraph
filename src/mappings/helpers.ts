ff0f674ee8926feff1034e4b8def01a7e1e87c0e
Malisa Pusonja <malisa.pusonja@gmail.com>
2020-11-20 21:21:19 +0100

    Update helpers.ts

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xfae6A4370a3499f363461647FD54d110b3C8DC64'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
